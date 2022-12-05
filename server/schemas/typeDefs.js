2db201edcb9f73e9f45ca54a56a3b53cd038d2d5
haiderali1998 <haiderali9823@outlook.com>
2022-12-05 18:47:31 +0000

    tyoedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,34 +1,47 @@
 const { gql } = require('apollo-server-express');
 
+//add ! to book(id & title) user(_id & name)
+
 const typeDefs = gql`
-  type Profile {
+  type User {
     _id: ID
     name: String
     email: String
     password: String
-    skills: [String]!
+    savedbooks: [String]!
+    bookcount: Int
   }
 
+type Book {
+  bookId: ID
+  authors: [String]
+  description: String
+  image: String
+  link: String
+  title: String     
+  
+}
   type Auth {
     token: ID!
-    profile: Profile
+    user: User
   }
 
   type Query {
-    profiles: [Profile]!
-    profile(profileId: ID!): Profile
-    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
-    me: Profile
+    me: User
+    
   }
 
   type Mutation {
-    addProfile(name: String!, email: String!, password: String!): Auth
-    login(email: String!, password: String!): Auth
+    addUser(name: String, email: String, password: String): Auth
+    login(email: String, password: String!): Auth
 
-    addSkill(profileId: ID!, skill: String!): Profile
-    removeProfile: Profile
-    removeSkill(skill: String!): Profile
+  
   }
 `;
 
 module.exports = typeDefs;
+
+
+// addSkill(profileId: ID!, skill: String!): Profile
+// removeProfile: Profile
+// removeSkill(skill: String!): Profile
--- server/server.js
+++ server/server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
+
 
 const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
 
-const app = express();
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -19,9 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
